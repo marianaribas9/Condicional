d368e187a5baef7fb65b8cb8a31aaee34a4536b2
Mariana <mariana.ribas.cardoso@escola.pr.gov.br>
2024-02-20 11:48:46 -0300

    Exerciciosdia20/02

---
+++ script.js
@@ -0,0 +1,9 @@
+let idade = 19;
+
+if (idade>18) {
+    console.log (`${idade} anos -> Maior de idade!`)
+} else if (idade == 18) {
+    console.log (`${idade} anos -> Está no limite!`)
+} else {
+    console.log (`${idade} anos -> Menor de idade!`)
+}
